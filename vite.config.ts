5324c01beddefadc428f052e12e681e7a72ee2c6
vivekgautamdev <pt.developervivek@gmail.com>
2026-05-01 16:41:04 +0530

    vite path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Maxure-Website/",
+  base: "/",
   optimizeDeps: {
     entries: ["src/main.tsx", "src/tempobook/**/*"],
   },
